2d84a8566050705b05a0f44e2a7ca4c1ee30149b
Rainner Lins <rainnerlins@gmail.com>
2018-03-03 12:22:53 -0500

    Updated demo app JS file

--- src/app.js
+++ src/app.js
@@ -4,36 +4,13 @@
 import './app.scss';
 import Scroller from './scroller';
 
-// setup scroller on target element
 const s = new Scroller();
-
-// when scroll position changes (throttled)
-s.onChange( pos => {
-  console.log( 'scrolling', pos );
-});
-
-// when scrolling up (triggered once)
-s.onUp( pos => {
-  console.log( 'going up', pos );
-});
-
-// when scrolling down (triggered once)
-s.onDown( pos => {
-  console.log( 'going down', pos );
-});
-
-// when scroll position is greater than a number (triggered once)
-s.moreThan( 200, pos => {
-  console.log( 'scrolled past 200', pos );
-});
-
-// when scroll position is less than a number (triggered once)
-s.lessThan( 200, pos => {
-  console.log( 'scrolled less than 200', pos );
-});
-
-// buttons
-let btns = document.querySelectorAll( '[data-scroll]' );
+const header = document.querySelector( '#header' );
+const statPosReal = document.querySelector( '#stat-pos-real' );
+const statPosSlow = document.querySelector( '#stat-pos-slow' );
+const statDirection = document.querySelector( '#stat-direction' );
+const statTrigger = document.querySelector( '#stat-trigger' );
+const btns = document.querySelectorAll( '[data-scroll]' );
 
 for ( let i = 0; i < btns.length; ++i ) {
   btns[ i ].addEventListener( 'click', e => {
@@ -41,4 +18,40 @@ for ( let i = 0; i < btns.length; ++i ) {
     s.jumpTo( e.target.getAttribute( 'data-scroll' ) || '' );
   });
 }
+
+s.onVisible( '.reveal', 'visible' );
+
+s.onScroll( e => {
+  statPosReal.textContent = e.pageY;
+});
+
+s.onChange( pos => {
+  statPosSlow.textContent = pos;
+});
+
+s.onUp( pos => {
+  header.classList.remove( 'hidden' );
+  statDirection.textContent = 'Going up';
+});
+
+s.onDown( pos => {
+  header.classList.add( 'hidden' );
+  statDirection.textContent = 'Going down';
+});
+
+s.moreThan( 20, pos => {
+  header.classList.add( 'solid' );
+});
+
+s.lessThan( 20, pos => {
+  header.classList.remove( 'solid' );
+});
+
+s.moreThan( 200, pos => {
+  statTrigger.textContent = 'More than 200px';
+});
+
+s.lessThan( 200, pos => {
+  statTrigger.textContent = 'Less than 200px';
+});
 
